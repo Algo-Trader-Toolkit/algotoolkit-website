077357a03db2a9a138dcc455f9e225517934789d
Algo-Trader-Toolkit <algo.toolkitt@gmail.com>
2025-12-14 17:41:18 +0300

    Add Google Analytics (G-XGFEVMT7PL)

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XGFEVMT7PL"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XGFEVMT7PL');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
